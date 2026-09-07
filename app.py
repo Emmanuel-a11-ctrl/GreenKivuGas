f54c208862c26f4b7e839d5d9e3e668bad885418
Emmanuel-a11-ctrl <emmanuel.niyonshuti@gasmeth.com>
2026-09-07 14:23:40 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -9,11 +9,15 @@ from dataclasses import dataclass
 from enum import Enum
 from typing import Dict, List, Optional, NamedTuple
 
-st.set_page_config(page_title="GreenKivuCNG | CNG Intelligence", layout="wide", page_icon="🌱")
+st.set_page_config(page_title="GreenKivuCNG | CNG Intelligence", layout="wide", page_icon="🍃")
 
 # --- Custom CSS for sidebar menu styling ---
 st.markdown("""
 <style>
+    body {
+        font-family: "Calibri", sans-serif;
+        font-weight: bold;
+    }
     .stRadio > label {
         font-size: 18px;
         font-weight: bold;
